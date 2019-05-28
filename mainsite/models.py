6bb4824db08a76896a227619d52f0a02163bbbe1
Zhong You <alkmo@yahoo.com.tw>
2019-05-28 10:16:13 +0800

    no message

--- mainsite/models.py
+++ mainsite/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)  # 文章標題
+    slug = models.CharField(max_length=200)  # 文章網址
+    body = models.TextField()   # 文章內容
+    pub_date = models.DateTimeField(default=timezone.now)   # 文章發表時間
+
+    class Meta:
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.title
--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k-p88429dh95w@6f3f1&hqnpmr9*-okmh-jws6b9o8)ct5^b!a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainsite',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'myblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
